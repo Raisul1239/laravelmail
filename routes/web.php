df8b9cf37c8be7fd04ae91c963cfe4e428fe025f
Raisul <bosrai15311239@gmail.com>
2020-02-23 02:09:23 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,9 @@
 |
 */
 
-use App\Http\Controllers\Ai_applcationsController;
-use App\Http\Controllers\NaturalhazardController;
 
-
-Route::get('/about', 'TeamController@show');
-
-Route::get('/products', 'ProductsController@show');
-Route::get('/project', 'ProjectController@show');
-Route::get('/research', 'ResearchController@show');
 Route::get('/contact', 'ContactController@show');
 Route::post('/sendemail/send', 'ContactController@send')->name('sendemail.send');
 
 
-Route::prefix('/services')->group(function () {
 
-    Route::get('/ai_applications', 'Ai_applcationsController@show');
-    Route::get('/engine_services', 'EngineServicesController@show');
-    Route::get('/enviro_services', 'EnviroservicesController@show');
-    Route::get('/management', 'ManagementController@show');
-    Route::get('/Natural_hazard', 'NaturalhazardController@show');
-});
-// Route::get('/about', function () {
-//     return view('about');
-// });
-// Route::get('/about', function () {
-//     return view('about');
-// });
